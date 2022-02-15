c8f53f8022407bdcc55959d4c3ddd297378e3032
Tarick <gabre.tarick@gmail.com>
2022-02-15 19:18:53 -0300

    conclui requisito 4

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
-const pegaCor = document.querySelectorAll('.color');
-console.log(pegaCor);
+const pegaPixel = document.querySelectorAll('.pixel');
+
+for (let i = 0; i < pegaPixel.length; i += 1) {
+    pegaPixel[i].style.backgroundColor = 'white';
+}
