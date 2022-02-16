3a12e7f1724501c61ae65a4eb56fc30d0b28a842
Tarick <gabre.tarick@gmail.com>
2022-02-16 16:58:01 -0300

    conclui requisito 8

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
-const pegaPixel = document.querySelectorAll('.pixel');
-
-for (let i = 0; i < pegaPixel.length; i += 1) {
-    pegaPixel[i].style.backgroundColor = 'white';
-}
-
-
 //requisito 7 consultei o código do Rodolfo Marques - Turma 15
 //https://github.com/tryber/sd-015-b-project-pixels-art/pull/119/commits/82965f04e64245a370921316c4bddd228e31263d
 let black = document.getElementById('preta');
 let blue = document.getElementById('azul');
 let gray = document.getElementById('cinza');
 let yellow = document.getElementById('amarela');
+
 
 black.addEventListener('click', selected);
 blue.addEventListener('click', selected);
@@ -22,3 +16,36 @@ function selected(event) {
     seleciona.classList.remove('selected')
     event.target.classList.add('selected');
 }
+
+//requisito 8. consultei o código do James Mayke - Turma 15
+//https://github.com/tryber/sd-015-b-project-pixels-art/pull/124/commits/11c2d00b6aef1e79198790c0329f1704a71b0417
+const pegaCores = document.querySelectorAll('.color');
+let pegaPixels = document.querySelectorAll('.pixel');
+
+for (let i = 0; i < pegaPixels.length; i += 1) {
+    pegaPixels[i].style.backgroundColor = 'rgb(255, 255, 255)';
+}
+
+for (let i = 0; i < pegaCores.length; i += 1) {
+    pegaCores[i].addEventListener('click', function(){
+
+        if (pegaCores[i].className !== 'selected') {
+            
+            for (let i = 0; i < pegaCores.length; i += 1) {
+                pegaCores[i].classList.remove('selected')
+            };
+        };
+        pegaCores[i].classList.add('selected')
+
+        for (let px = 0; px < pegaPixels.length; px += 1) {
+            pegaPixels[px].addEventListener('click', function() {
+                pegaPixels[px].style.backgroundColor = pegaCores[i].style.backgroundColor;
+            })
+        }
+    });
+};
+
+
+
+
+
