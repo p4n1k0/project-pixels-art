cf7a0fa8b9ba93f963d4a6cd605bd7978649bb3d
Tarick <gabre.tarick@gmail.com>
2022-02-17 22:36:43 -0300

    conclui requisito 8 finalmente

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
 //requisito 2 usando JS
+//elementos html
 const cor1 = document.getElementById('preta');
 const cor2 = document.getElementById('azul');
 const cor3 = document.getElementById('cinza');
 const cor4 = document.getElementById('amarela');
+const pixelBoard = document.getElementById('pixel-board');
 
 cor1.style.backgroundColor = 'black';
 cor2.style.backgroundColor = 'blue';
 cor3.style.backgroundColor = 'gray';
 cor4.style.backgroundColor = 'yellow';
 
-//requisito 7 usando JS
+//requisito 7
 cor1.addEventListener('click', adicionaClasseSelected);
 cor2.addEventListener('click', adicionaClasseSelected);
 cor3.addEventListener('click', adicionaClasseSelected);
 cor4.addEventListener('click', adicionaClasseSelected);
+pixelBoard.addEventListener('click', mudaCor);
 
 function adicionaClasseSelected(event) {
-    const verificaSelected = document.querySelector('.selected');
+    const verificaSelected = document.querySelector('.selected'); //elemnto html
     verificaSelected.classList.remove('selected');
     event.target.classList.add('selected');
 }
+//requisito 8
+function mudaCor(event) {
+    const corSelecionada = document.querySelector('.selected').style.backgroundColor;
+    event.target.style.backgroundColor = corSelecionada;
+}
+
