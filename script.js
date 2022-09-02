bfbcb3561b71731bee1599a70f352499a36b7f88
Tarick <gabre.tarick@gmail.com>
2022-09-02 11:55:53 -0300

    commit para publicacao

--- script.js
+++ script.js
@@ -1,40 +1,40 @@
-//requisito 2 usando JS
-//elementos html
-const cor1 = document.getElementById('preta');
-const cor2 = document.getElementById('azul');
-const cor3 = document.getElementById('cinza');
-const cor4 = document.getElementById('amarela');
+// requisito 2 usando JS
+// elementos html
+const color1 = document.getElementById('black');
+const color2 = document.getElementById('blue');
+const color3 = document.getElementById('gray');
+const color4 = document.getElementById('yellow');
 const pixelBoard = document.getElementById('pixel-board');
 const pixels = document.getElementsByClassName('pixel');
 const clearBoard = document.getElementById('clear-board');
 
-cor1.style.backgroundColor = 'black';
-cor2.style.backgroundColor = 'blue';
-cor3.style.backgroundColor = 'gray';
-cor4.style.backgroundColor = 'yellow';
+color1.style.backgroundColor = 'black';
+color2.style.backgroundColor = 'blue';
+color3.style.backgroundColor = 'gray';
+color4.style.backgroundColor = 'yellow';
 
-cor1.addEventListener('click', adicionaClasseSelected);
-cor2.addEventListener('click', adicionaClasseSelected);
-cor3.addEventListener('click', adicionaClasseSelected);
-cor4.addEventListener('click', adicionaClasseSelected);
-pixelBoard.addEventListener('click', mudaCor);
-clearBoard.addEventListener('click', botaoLimpa);
-
-
-function adicionaClasseSelected(event) {
-    const verificaSelected = document.querySelector('.selected'); //elemento html
-    verificaSelected.classList.remove('selected');
-    event.target.classList.add('selected');
-}
-//requisito 8
-function mudaCor(event) {
-    const corSelecionada = document.querySelector('.selected').style.backgroundColor; //elemento html
-    event.target.style.backgroundColor = corSelecionada;
+function addClassSelected(addClass) {
+  const verifySelected = document.querySelector('.selected'); // elemento html
+  verifySelected.classList.remove('selected');
+  addClass.target.classList.add('selected');
 }
 
-//requisito 9
-function botaoLimpa() {
-    for (let i = 0; i < pixels.length; i += 1) {
-        pixels[i].style.backgroundColor = 'white';
-    }
+// requisito 8
+function changeColor(change) {
+  const selectColor = document.querySelector('.selected').style.backgroundColor; // elemento html
+  change.target.style.backgroundColor = selectColor;
 }
+
+// requisito 9
+function clearBtn() {
+  for (let i = 0; i < pixels.length; i += 1) {
+    pixels[i].style.backgroundColor = 'white';
+  }
+}
+
+color1.addEventListener('click', addClassSelected);
+color2.addEventListener('click', addClassSelected);
+color3.addEventListener('click', addClassSelected);
+color4.addEventListener('click', addClassSelected);
+pixelBoard.addEventListener('click', changeColor);
+clearBoard.addEventListener('click', clearBtn);
