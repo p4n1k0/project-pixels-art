b6729b648dae49b1ad1a88f7057054b2c2aa503d
p4n1k0 <gabre.tarick@gmail.com>
2025-04-01 18:36:11 -0300

    quadro de pixels definido pela pessoa usuaria e limita o tamanho minimo e maximo do board

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ const color1 = document.getElementById('black');
 const color2 = document.getElementById('blue');
 const color3 = document.getElementById('green');
 const color4 = document.getElementById('yellow');
-const pixelBoard = document.getElementById('pixel-board');
-const pixels = document.getElementsByClassName('pixel');
+let pixelBoard = document.getElementById('pixel-board');
+let pixels = document.getElementsByClassName('pixel');
 const clearBoard = document.getElementById('clear-board');
+let boardSize = document.getElementById('board-size');
+let buttonVqv = document.getElementById('generate-board');
 
 color1.style.backgroundColor = 'black';
 color2.style.backgroundColor = 'blue';
@@ -17,20 +19,20 @@ function addClassSelected(addClass) {
   const verifySelected = document.querySelector('.selected'); // elemento html
   verifySelected.classList.remove('selected');
   addClass.target.classList.add('selected');
-}
+};
 
 // requisito 8
 function changeColor(change) {
   const selectColor = document.querySelector('.selected').style.backgroundColor; // elemento html
   change.target.style.backgroundColor = selectColor;
-}
+};
 
 // requisito 9
 function clearBtn() {
   for (let i = 0; i < pixels.length; i += 1) {
     pixels[i].style.backgroundColor = 'white';
-  }
-}
+  };
+};
 
 color1.addEventListener('click', addClassSelected);
 color2.addEventListener('click', addClassSelected);
@@ -38,3 +40,31 @@ color3.addEventListener('click', addClassSelected);
 color4.addEventListener('click', addClassSelected);
 pixelBoard.addEventListener('click', changeColor);
 clearBoard.addEventListener('click', clearBtn);
+
+function createBoard (size) {
+  pixelBoard.innerHTML = '';
+
+  for (let i = 0; i < size; i += 1) {
+    let sectionLine = document.createElement('section');
+    sectionLine.className = 'line';
+    pixelBoard.appendChild(sectionLine);
+    for (let index = 0; index < size; index += 1) {
+      pixels = document.createElement('div');
+      sectionLine.appendChild(pixels);
+      pixels.className = 'pixel';
+    };
+  };
+};
+createBoard(5);
+
+buttonVqv.addEventListener('click', function() {
+  let size = boardSize.value;
+  if (size == '') alert('Board inválido!');
+  else if (size < 5) {
+    size = 5;
+    createBoard(size);
+  } else if (size > 50) {
+    size = 50;
+    createBoard(size);
+  } else createBoard(size);
+});
